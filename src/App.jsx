d4e2032c5918e9eb471ff1d7f2fc2d73e2aa4711
SergioQuinteroC <sergio.quinteroc@udea.edu.co>
2023-01-04 16:52:31 -0500

    ADD api response

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@mui/material";
 import Searcher from "./containers/Searcher";
-import { useState } from "react";
+import { getGitHubUser } from "./services/users";
 
 const App = () => {
   const [inputUser, setInputUser] = useState("octocat");
-  const [userStater, userState] = useState("inputUser");
+  const [userState, setUserState] = useState("inputUser");
+  const [notFound, setNotFound] = useState(false);
+
+  const gettingUser = async (user) => {
+    const userResponse = await getGitHubUser(user);
+
+    if (userState === "octocat") {
+      localStorage.setItem("octocat", userResponse);
+    }
+
+    if (userResponse.message === "Not Found") {
+      const { octocat } = localStorage;
+      setInputUser(octocat);
+      setNotFound(true);
+    } else {
+      setUserState(userResponse);
+    }
+  };
+  console.log(userState);
+
+  useEffect(() => {
+    gettingUser(inputUser);
+  }, [inputUser]);
 
   return (
     <Container
